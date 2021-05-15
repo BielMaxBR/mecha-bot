4a3ab33bac62c0331e1b56cd9b09dcdf588a52ff
Gabriel Maximo <gbielmaximo@gmail.com>
2021-05-15 23:00:57 +0000

    atividade rotacionada

--- bot.js
+++ bot.js
@@ -4,6 +4,14 @@ const prefix = '!!'
 
 client.on('ready', ()=>{
   client.user.setActivity('mecha bot na área', 'COMPETING')
+  let activities = [
+    "amogus",
+    "minescrefts",
+    "fogo gratis",
+    "tretris"
+  ]
+  i = 0;
+  setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 3000);
 })
 
 client.on('message',message => {

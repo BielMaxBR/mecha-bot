6acb0d5ada31303228c50917d8d877c0e46c78e8
* <gbielmaximo@gmail.com>
2021-05-16 11:57:44 -0300

    tirando bug na checagem de comando

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,18 +1,22 @@
 const fetch = require('node-fetch');
-const cheerio = require('cheerio')
+//const cheerio = require('cheerio')
 
 module.exports = async function({message, args}) {
   let content = ''
   args.forEach(val => content += " "+val.toString())
-  fetch('https://pt.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=json&search='+content).then(response => response.json())
+  const url = 'https://pt.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=json&search='+content
+
+  console.log(url)
+
+  fetch(url).then(response => response.json())
   .then(async response =>{
     const link = response[3]
 
     const html = await fetch(link)
 
-    let $ = cheerio.load(html)
+  //  let $ = cheerio.load(html)
 
-    console.log($)
+//    console.log($)
     await message.channel.send(link)
   })
 }

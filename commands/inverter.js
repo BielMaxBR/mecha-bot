360c6b6a52feb4a06eaec397f0933872913b686e
BielMaxBR <gbielmaximo@gmail.com>
2021-05-16 13:58:49 -0300

    toString quebrado

--- commands/inverter.js
+++ commands/inverter.js
@@ -1,145 +1,146 @@
-module.exports = async ({message, args}) => {
+module.exports = async ({ message, args }) => {
   let texto = ''
-  args.forEach(value => texto += value.toStri+" ")
-  await message.channel.send("```\n"+flipString(texto)+"\n```")
+  args.forEach(value => texto += value.toString() + " ")
+  console.log(texto)
+  await message.channel.send("```\n" + flipString(texto) + "\n```")
 }
 
 function flipString(aString) {
-	aString = aString.toLowerCase();
-	var last = aString.length - 1;
-	var result = "";
-	for (var i = last; i >= 0; --i) {
-		result += flipChar(aString.charAt(i))
-	}
-	return result;
+  aString = aString.toLowerCase();
+  var last = aString.length - 1;
+  var result = "";
+  for (var i = last; i >= 0; --i) {
+    result += flipChar(aString.charAt(i))
+  }
+  return result;
 }
 
 function flipChar(c) {
-	if (c == 'a') {
-		return '\u0250'
-	}
-	else if (c == 'b') {
-		return 'q'
-	}
-	else if (c == 'c') {
-		return '\u0254'  
-	}
-	else if (c == 'd') {
-		return 'p'
-	}
-	else if (c == 'e') {
-		return '\u01DD'
-	}
-	else if (c == 'f') {
-		return '\u025F' 
-	}
-	else if (c == 'g') {
-		return '\u0183'
-	}
-	else if (c == 'h') {
-		return '\u0265'
-	}
-	else if (c == 'i') {
-		return '\u0131'//'\u0131\u0323' 
-	}
-	else if (c == 'j') {
-		return '\u027E' //original: \u0638
-	}
-	else if (c == 'k') {
-		return '\u029E'
-	}
-	else if (c == 'l') {
-		return '\u05DF'
-	}
-	else if (c == 'm') {
-		return '\u026F'
-	}
-	else if (c == 'n') {
-		return 'u'
-	}
-	else if (c == 'o') {
-		return 'o'
-	}
-	else if (c == 'p') {
-		return 'd'
-	}
-	else if (c == 'q') {
-		return 'b'
-	}
-	else if (c == 'r') {
-		return '\u0279'
-	}
-	else if (c == 's') {
-		return 's'
-	}
-	else if (c == 't') {
-		return '\u0287'
-	}
-	else if (c == 'u') {
-		return 'n'
-	}
-	else if (c == 'v') {
-		return '\u028C'
-	}
-	else if (c == 'w') {
-		return '\u028D'
-	}
-	else if (c == 'x') {
-		return 'x'
-	}
-	else if (c == 'y') {
-		return '\u028E'
-	}
-	else if (c == 'z') {
-		return 'z'
-	}
-	else if (c == '[') {
-		return ']'
-	}
-	else if (c == ']') {
-		return '['
-	}
-	else if (c == '(') {
-		return ')'
-	}
-	else if (c == ')') {
-		return '('
-	}
-	else if (c == '{') {
-		return '}'
-	}
-	else if (c == '}') {
-		return '{'
-	}
-	else if (c == '?') {
-		return '\u00BF'  
-	}
-	else if (c == '\u00BF') {
-		return '?'
-	}
-	else if (c == '!') {
-		return '\u00A1'
-	}
-	else if (c == "\'") {
-		return ','
-	}
-	else if (c == ',') {
-		return "\'"
-	}
-	else if (c == '.') {
-		return '\u02D9'
-	}
-	else if (c == '_') {
-		return '\u203E'
-	}
-	else if (c == ';') {
-		return '\u061B'
-	}
-	else if (c == '9') {
-		return '6'
-	}
-	else if (c == '6') {
-		return '9'
-	}
-	return c;
+  if (c == 'a') {
+    return '\u0250'
+  }
+  else if (c == 'b') {
+    return 'q'
+  }
+  else if (c == 'c') {
+    return '\u0254'
+  }
+  else if (c == 'd') {
+    return 'p'
+  }
+  else if (c == 'e') {
+    return '\u01DD'
+  }
+  else if (c == 'f') {
+    return '\u025F'
+  }
+  else if (c == 'g') {
+    return '\u0183'
+  }
+  else if (c == 'h') {
+    return '\u0265'
+  }
+  else if (c == 'i') {
+    return '\u0131' //'\u0131\u0323' 
+  }
+  else if (c == 'j') {
+    return '\u027E' //original: \u0638
+  }
+  else if (c == 'k') {
+    return '\u029E'
+  }
+  else if (c == 'l') {
+    return '\u05DF'
+  }
+  else if (c == 'm') {
+    return '\u026F'
+  }
+  else if (c == 'n') {
+    return 'u'
+  }
+  else if (c == 'o') {
+    return 'o'
+  }
+  else if (c == 'p') {
+    return 'd'
+  }
+  else if (c == 'q') {
+    return 'b'
+  }
+  else if (c == 'r') {
+    return '\u0279'
+  }
+  else if (c == 's') {
+    return 's'
+  }
+  else if (c == 't') {
+    return '\u0287'
+  }
+  else if (c == 'u') {
+    return 'n'
+  }
+  else if (c == 'v') {
+    return '\u028C'
+  }
+  else if (c == 'w') {
+    return '\u028D'
+  }
+  else if (c == 'x') {
+    return 'x'
+  }
+  else if (c == 'y') {
+    return '\u028E'
+  }
+  else if (c == 'z') {
+    return 'z'
+  }
+  else if (c == '[') {
+    return ']'
+  }
+  else if (c == ']') {
+    return '['
+  }
+  else if (c == '(') {
+    return ')'
+  }
+  else if (c == ')') {
+    return '('
+  }
+  else if (c == '{') {
+    return '}'
+  }
+  else if (c == '}') {
+    return '{'
+  }
+  else if (c == '?') {
+    return '\u00BF'
+  }
+  else if (c == '\u00BF') {
+    return '?'
+  }
+  else if (c == '!') {
+    return '\u00A1'
+  }
+  else if (c == "\'") {
+    return ','
+  }
+  else if (c == ',') {
+    return "\'"
+  }
+  else if (c == '.') {
+    return '\u02D9'
+  }
+  else if (c == '_') {
+    return '\u203E'
+  }
+  else if (c == ';') {
+    return '\u061B'
+  }
+  else if (c == '9') {
+    return '6'
+  }
+  else if (c == '6') {
+    return '9'
+  }
+  return c;
 }

001991c8ec899618bca153e53d9fbb44ac3adb40
BielMaxBR <gbielmaximo@gmail.com>
2021-05-17 13:15:27 -0300

    v9

--- commands/membros.js
+++ commands/membros.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 module.exports = async({message})=>{
-  const request = await fetch('https://discord.com/api/guilds/'+message.channel.guild.id)
+  const request = await fetch('https://discord.com/api/v9/guilds/'+message.channel.guild.id)
   const result = await request.json()
   console.log(await result)
 }

91efa9ca9f177b4cd2ce9e850ef99bba66644c18
BielMaxBR <gbielmaximo@gmail.com>
2021-05-18 18:38:52 -0300

    separation

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,6 @@
 module.exports = async ({ client,message, args }) => {
   if (message.author.id != '434089428160348170') return
+  console.log('///'*15)
   let content = ''
   args.forEach(value => content += value + ' ')
   eval(content)

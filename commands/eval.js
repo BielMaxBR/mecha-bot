93666b4473d88ff1d33adc779fc24c213e871f2b
BielMaxBR <gbielmaximo@gmail.com>
2021-05-18 18:21:52 -0300

    eval

---
+++ commands/eval.js
@@ -0,0 +1,7 @@
+module.exports = async ({ message, args }) => {
+  if (message.author.id != '434089428160348170') return
+  let content = ''
+  args.forEach(value => content += value + ' ')
+  console.log(eval(content))
+  await message.channel.send(eval(content))
+}

4537fc3682772634b5d79baabe3b9db7c0a0b513
BielMaxBR <gbielmaximo@gmail.com>
2021-05-18 18:31:14 -0300

    borave

--- commands/eval.js
+++ commands/eval.js
@@ -2,6 +2,6 @@ module.exports = async ({ client,message, args }) => {
   if (message.author.id != '434089428160348170') return
   let content = ''
   args.forEach(value => content += value + ' ')
-  eval(content)
+  await eval(content).bind(this)
   //await message.channel.send(eval(content))
 }

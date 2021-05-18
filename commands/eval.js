3965f6aee63e6e85f3304ccf797ae1854e6176d6
BielMaxBR <gbielmaximo@gmail.com>
2021-05-18 18:37:05 -0300

    vrau

--- commands/eval.js
+++ commands/eval.js
@@ -2,6 +2,6 @@ module.exports = async ({ client,message, args }) => {
   if (message.author.id != '434089428160348170') return
   let content = ''
   args.forEach(value => content += value + ' ')
-  await eval(content).bind(this)
+  eval(content)
   //await message.channel.send(eval(content))
 }

28e8ebb9d51a6a55676cc01559ae28aebf0326c8
BielMaxBR <gbielmaximo@gmail.com>
2021-06-04 00:20:58 -0300

    '-'

--- commands/musica/searchMusic.js
+++ commands/musica/searchMusic.js
@@ -3,9 +3,10 @@ module.exports = (music) =>{
   return new Promise(async (resolve,reject)=>{
     let res = await search(music)
     let url = res.videos[0].url
+    let title = res.videos[0].title
     console.log(url)
     if(url) {
-    resolve(res.videos[0])
+    resolve({url,title})
       
     } else {
       reject(null)

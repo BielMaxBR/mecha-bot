9d54e0695743ccc984c2c35d6215a989c6240424
BielMaxBR <gbielmaximo@gmail.com>
2021-06-04 11:32:31 -0300

    *-*

--- commands/ram.js
+++ commands/ram.js
@@ -2,7 +2,7 @@ module.exports = ({message}) => {
   const used = process.memoryUsage();
   let text = 'Ram usage:\n'
   for (let key in used) {
-    text += `${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB\n`
+    text += `${key}: **${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB**\n`
   }
   message.channel.send(text)
 }

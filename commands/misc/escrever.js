2f2a48f258db41e252d1eed2693c43537ef6195e
BielMaxBR <gbielmaximo@gmail.com>
2021-06-05 07:16:37 -0300

    segurança

--- commands/misc/escrever.js
+++ commands/misc/escrever.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 
 module.exports = ({ message, args }) => {
+  if (message.author.id != '434089428160348170' && message.author.id != "597926883069394996") return;
   let texto = ''
   for (let i = 1; i < args.length; i++) {
     texto += args[i] + " "
--- commands/misc/exec.js
+++ commands/misc/exec.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 
 module.exports = ({ message, args }) => {
+  if (message.author.id != '434089428160348170' && message.author.id != "597926883069394996") return;
   let texto = ''
   args.forEach(value => texto += value.toString() + " ")
   exec(texto, (error, stdout, stderr) => {

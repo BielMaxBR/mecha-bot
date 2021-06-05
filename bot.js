90f55557bd51a4d602a1b154ca8a737a9ed19ad9
BielMaxBR <gbielmaximo@gmail.com>
2021-06-05 06:56:50 -0300

    menos pior

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { Client } = require("discord.js-light")
 const { opendir, readdir } = require("fs/promises")
+const { existsSync } = require('fs')
 const client = new Client({
   cacheGuilds: true,
   cacheChannels: true,
@@ -33,28 +34,26 @@ client.on('message', async message => {
   const args = content.slice(prefix.length).trim().split(' ');
   const command = args.shift().toLowerCase();
   const dirs = await opendir('./commands')
-  
+  // itera sobre cada categoria
   for await (let dir of dirs) {
     const category = Object.values(dir)
-    
-    if (category[1] == 1) return
-    let commands = await opendir('./commands/' + category[0])
-    
-    for await (let command of commands) {
-      let command = Object.values(command)
-      
-      if (command[1] == 2) return
-      let path = './commands/' + category[0] + "/" + command[0]
-      
-      try {
-        require(path)({ client, message, args })
-        
+
+    if (category[1] == 2) {
+      let commands = await opendir('./commands/' + category[0])
+      let path = './commands/' + category[0] + "/" + command + ".js"
+      // roda o arquivo encontrado
+      if (existsSync(path)) {
+        try {
+          require(path)({ client, message, args })
+
+        }
+        catch (err) {
+          console.log(err)
+          message.channel.send(err)
+
+        }
       }
-      catch (err) {
-        console.log(err)
-        message.channel.send(err)
-        
-      }
+
     }
   }
   /*const path = `./commands/${command}.js`

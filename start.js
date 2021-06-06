bb2ff441fc6e1ccd9f89e15415da4ae9855c36ef
BielMaxBR <gbielmaximo@gmail.com>
2021-06-06 14:10:03 -0300

    ...

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const server = require("./server/server.js")
 const bot = require("./bot.js")
-
+console.log(Object.keys(bot))
 require('dotenv').config()
 //server.listen(process.env.PORT || 5005)
 bot.login(process.env.TOKEN);

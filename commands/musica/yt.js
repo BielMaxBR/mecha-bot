e994cc821b7d8b7ca3290fb25f26d9656d6192ea
BielMaxBR <gbielmaximo@gmail.com>
2021-06-07 13:41:48 -0300

    opa

--- commands/musica/yt.js
+++ commands/musica/yt.js
@@ -3,18 +3,16 @@ const ytdl = require('ytdl-core');
 const { MessageEmbed } = require('discord.js')
 module.exports = async ({ message, args }) => {
   let text = ''
-  const con = await message.member.voice.channel.join()
   args.forEach(v => text += v + " ")
-  con.play(ytdl(text)).setVolumeDecibels(10)
-  // let r = await search(text)
-  // let video = r.videos[0]
-  // const msg = new MessageEmbed()
-  //     .setColor('#ff0000')
-  //     .setTitle(video.title)
-  //     .setDescription(video.description)
-  //     .setThumbnail(video.image)
-  //     .setFooter('Mecha-bot')
-  //     .setURL(video.url)
+  let r = await search(text)
+    let video = r.videos[0]
+    const msg = new MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle(video.title)
+        .setDescription(video.description)
+        .setThumbnail(video.image)
+        .setFooter('Mecha-bot')
+        .setURL(video.url)
 
-  // await message.channel.send(msg)
+    await message.channel.send(msg)
 }

1f7c8b823ec69271b88b6a792147c6bb890ecad6
BielMaxBR <gbielmaximo@gmail.com>
2021-06-08 10:54:49 -0300

    api test

---
+++ commands/misc/emojis.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+module.exports = async ({ message }) => {
+  const request = await fetch('https://discord.com/api/v9/guilds/' + message.channel.guild.id + '/emojis', {
+    headers: {
+      Authorization: `Bot ${process.env.TOKEN}`,
+    },
+  })
+  const result = await request.json()
+  console.log(await result)
+  await message.channel.send(JSON.stringlify(await result))
+}

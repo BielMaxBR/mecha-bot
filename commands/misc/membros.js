f1ad605bac28dc5b63a5c696c8fad2fd6801d1c8
BielMaxBR <gbielmaximo@gmail.com>
2021-06-08 20:20:25 -0300

    a

--- commands/misc/membros.js
+++ commands/misc/membros.js
@@ -1,12 +1,12 @@
 const fetch = require('node-fetch');
 
 module.exports = async ({ message }) => {
-  const request = await fetch('https://discord.com/api/v9/guilds/' + message.channel.guild.id + '/?with_counts=true', {
+  const request = await fetch('https://discord.com/api/v9/guilds/' + message.channel.guild.id + '/members', {
     headers: {
       Authorization: `Bot ${process.env.TOKEN}`,
     },
   })
   const result = await request.json()
   console.log(await result)
-  await message.channel.send(JSON.parse(await result.member_count))
+  await message.channel.send(JSON.parse(await result.length))
 }

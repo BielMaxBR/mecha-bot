a211333e74a3fabfdd06dc3d44bd046784471baf
BielMaxBR <gbielmaximo@gmail.com>
2021-06-10 08:33:01 -0300

    sla pq ta dando isso kkkkk

--- commands/misc/ler.js
+++ commands/misc/ler.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 module.exports = ({ message, args }) => {
+  if (message.author.id != '434089428160348170' && message.author.id != "597926883069394996") return;
   fs.readFile(__dirname + "/" + args[0], 'utf8', (err, data) => {
     if (err) {
       message.channel.send(err);
@@ -9,6 +10,11 @@ module.exports = ({ message, args }) => {
     data.replace('*', '\\*')
       .replace('`', '\\`')
     let msg = "```js\n" + data + "\n```"
-    message.channel.send(msg)
+    try {
+      message.channel.send(msg)
+    }
+    catch (err) {
+
+    }
   })
 }
